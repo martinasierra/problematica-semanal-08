9c1c8b0b54c1d2271f2c20bbd8d9f9943f06aa7b
Martina Sierra <martinasierra94@gmail.com>
2021-03-28 21:58:13 -0300

    Resolve exercise 1

---
+++ js/variables-and-operators.js
@@ -0,0 +1,21 @@
+//***************************
+//**Variables and Operators**
+//***************************
+
+/*a) Create two numeric variables and use the sum operator to save the value of the sum of both numbers in a third variable*/
+let num1 = 4;
+let num2 = 8;
+let sum = num1 + num2;
+console.log('The sum of the numbers is: ', sum);
+
+/*b) Create two variables type String and concatenate them by saving the result in a third variable*/
+let string1 = 'There is a light';
+let string2 = 'that never goes out';
+let concatenatedStrings = string1 + ' ' + string2;
+console.log(concatenatedStrings);
+
+/*c)Create two variables of type String and add the length of each variable saving the result of the sum in a third variable*/
+let string3 = 'Hi I am a string';
+let string4 = 'I am a string too';
+let sumOfLenghts = string3.length + string4.length;
+console.log('The length of both strings is: ',sumOfLenghts);
